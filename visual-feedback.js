7e9154ca164f64d97d842cc5365de497fb6b81a2
Jessica Yang <jessica.yang@cca.edu>
2018-02-11 13:25:50 -0800

    Update visual-feedback.js

--- visual-feedback.js
+++ visual-feedback.js
@@ -1,1 +1,113 @@
+var freqA = 174;
+var freqS = 196;
+var freqD = 220;
+var freqF = 246;
 
+var oscA, oscS, oscD, oscF;
+
+var playingA = false;
+var playingS = false;
+var playingD = false;
+var playingF = false;
+
+function setup() {
+  backgroundColor = color(255, 0, 255);
+  textAlign(CENTER);
+  
+  oscA = new p5.Oscillator();
+  oscA.setType('triangle');
+  oscA.freq(freqA);
+  oscA.amp(0);
+  oscA.start();
+  
+  oscS = new p5.Oscillator();
+  oscS.setType('triangle');
+  oscS.freq(freqS);
+  oscS.amp(0);
+  oscS.start();
+  
+  oscD = new p5.Oscillator();
+  oscD.setType('triangle');
+  oscD.freq(freqD);
+  oscD.amp(0);
+  oscD.start();
+  
+  oscF = new p5.Oscillator();
+  oscF.setType('triangle');
+  oscF.freq(freqF);
+  oscF.amp(0);
+  oscF.start();
+}
+
+function draw() {
+  if (playingA, playingS, playingD, playingF) {
+    background(0, 255, 255);
+  } else {
+    background(255, 0, 255);
+  }
+  text('click here,\nthen press A/S/D/F\n keys to play', width / 2, 40);
+}
+
+function keyPressed() {
+  print("got key press for ", key);
+  var osc;
+  if (key == 'A') {
+    osc = oscA;
+    playingA = true;
+  } else if (key == 'S') {
+    osc = oscS;
+    playingS = true;
+  } else if (key == 'D') {
+    osc = oscD;
+    playingD = true;
+  } else if (key == 'F') {
+    osc = oscF;
+    playingF = true;
+  }
+  if (osc) {
+    osc.amp(0.5, 0.1);
+  }
+
+}
+  function draw() {
+  background(255);
+  noStroke();
+  fill("green");
+  if (playingA) {
+    ellipse(0, 100, width, height);
+  }
+  if (playingS) {
+    fill("red");
+    ellipse(100, 20, 80, 80);
+  }
+    if (playingD) {
+    fill("blue");
+    ellipse(10, 10, width, height);
+  }
+  if (playingF) {
+  fill("yellow");
+  ellipse(90, 100, 60, 60);
+  }
+}
+    
+
+function keyReleased() {
+  print("got key release for ", key);
+  var osc;
+  if (key == 'A') {
+    osc = oscA;
+  } else if (key == 'S') {
+    osc = oscS;
+  } else if (key == 'D') {
+    osc = oscD;
+  } else if (key == 'F') {
+    osc = oscF;
+  }
+  if (osc) {
+    osc.amp(0, 0.5);
+    playingA = false;
+		playingS = false;
+		playingD = false;
+		playingF = false;
+  }
+}
